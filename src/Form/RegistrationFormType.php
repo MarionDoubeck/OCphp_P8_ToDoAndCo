27810ecfd1e36dd2ee39194f721b40ec02d33a0b
Marion <mariondoubeck@yahoo.fr>
2024-02-01 09:50:22 +0100

    add repeat psw

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\BoolType;
@@ -15,6 +16,7 @@ use Symfony\Component\Validator\Constraints\Choice;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\IdenticalTo;
 
 class RegistrationFormType extends AbstractType
 {
@@ -54,14 +56,22 @@ class RegistrationFormType extends AbstractType
                 ]
                 
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // Instead of being set onto the object directly, this is read and encoded in the controller.
-                'label' => 'Mot de passe',
-                'mapped' => false,
-                'attr' => [
-                    'autocomplete' => 'new-password',
-                    'class' => 'form-control my-2',
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'class' => 'form-control my-2',
+                    ],
                 ],
+                'second_options' => [
+                    'label' => 'Répéter le mot de passe',
+                    'attr' => [
+                        'class' => 'form-control my-2',
+                    ],
+                ],
+                'mapped' => false,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -72,8 +82,12 @@ class RegistrationFormType extends AbstractType
                         // Max length allowed by Symfony for security reasons.
                         'max' => 4096,
                     ]),
-                ]
-            ])
+                    new IdenticalTo([
+                        'propertyPath' => 'plainPassword',
+                        'message' => 'Les mots de passe ne correspondent pas.',
+                    ]),
+                ],
+            ]);
         ;
     }
 
