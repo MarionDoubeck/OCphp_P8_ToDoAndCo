99014da40cc25487d6c6ce5f97802b7f8dd446a4
Marion <mariondoubeck@yahoo.fr>
2024-02-01 21:27:27 +0100

    admin can add, edit and delete users

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -6,29 +6,25 @@ use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\BoolType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
-class EditUserFormType extends AbstractType
+class UserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('username', TextType::class, [
-                'mapped' => false,
-                'required' => false,
                 'attr' => [
                     'class' => 'form-control my-2'
                 ],
                 'label' => 'Nom d\'utilisateur'
             ])
             ->add('email', EmailType::class, [
-                'mapped' => false,
-                'required' => false,
                 'attr' => [
                     'class' => 'form-control my-2'
                 ],
@@ -40,25 +36,26 @@ class EditUserFormType extends AbstractType
                 'attr' => [
                     'class' => 'my-2'
                 ],
-                'label' => 'Voulez-vous vous inscrire en tant qu\'administrateur ? Oui '
+                'label' => 'Voulez-vous inscrire cet utilisateur en tant qu\'administrateur ? Oui '
             ])
-            ->add('plainPassword', PasswordType::class, [
-                'label' => 'Modifiez votre mot de passe ou laissez vide pour ne pas le modifier',
-                'required' => false,
-                'mapped' => false,
-                'attr' => [
-                    'autocomplete' => 'new-password',
-                    'class' => 'form-control my-2',
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'class' => 'form-control my-2',
+                    ],
                 ],
-                'constraints' => [
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // Max length allowed by Symfony for security reasons.
-                        'max' => 4096,
-                    ]),
-                ]
-            ])
+                'second_options' => [
+                    'label' => 'Répéter le mot de passe',
+                    'attr' => [
+                        'class' => 'form-control my-2',
+                    ],
+                ],
+                'mapped' => false,
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+            ]);
         ;
     }
 
