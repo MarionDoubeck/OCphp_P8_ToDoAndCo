5061f0404656d114d6f3d3dc51a1118a16d44002
Marion <mariondoubeck@yahoo.fr>
2024-02-06 15:54:58 +0100

    bug with symfony 7.0, downgraded to 6.3

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -9,7 +9,6 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -36,9 +35,7 @@ class UsersAuthenticator extends AbstractLoginFormAuthenticator
             new UserBadge($username),
             new PasswordCredentials($request->request->get('password', '')),
             [
-                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-                new RememberMeBadge(),
-            ]
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),            ]
         );
     }
 
